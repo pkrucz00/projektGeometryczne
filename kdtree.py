61184a31bb7b1d3633b03ce8dcf0b8e1ac2835a7
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2020-12-22 19:05:53 +0100

    KDTree implementation (without searching)

--- kdtree.py
+++ kdtree.py
@@ -1,4 +1,18 @@
-class Leaf:
+from math import inf, ceil
+
+class Range:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def isContainedIn(self, other):
+        return other.x1 <= self.x1 and self.x2 <= other.x2\
+            and other.y1 <= self.y1 and self.y2 <= self.y2
+
+
+class LeafNode:
     def __init__(self, point):
         self.point = point
 
@@ -19,22 +33,23 @@ class KDTree:
     def __initAux(self, pointsXSorted, pointsYSorted, depth):
         axis = depth % 2
 
-        if pointsXSorted == 0:
-            return None
-        if pointsXSorted == 1:
+        if len(pointsXSorted) == 1:
             singleton = pointsXSorted[0]  # in this case same as pointsYSorted
-            return Leaf(singleton)
+            return LeafNode(singleton)
 
         leftXSorted, rightXSorted, leftYSorted, rightYSorted = self.__split(pointsXSorted, pointsYSorted, axis)
         splitLine = leftXSorted[-1][0] if axis == 0 else leftYSorted[-1][0]
-        return Node(splitLine, self.__initAux(leftXSorted, leftYSorted), self.__initAux(rightXSorted, rightYSorted))
+        return Node(splitLine,
+                    self.__initAux(leftXSorted, leftYSorted, depth+1),
+                    self.__initAux(rightXSorted, rightYSorted, depth+1))
 
 
     def __split(self, x_sorted, y_sorted, axis):
         arrWithMedian = x_sorted if axis == 0 else y_sorted
         otherArr = y_sorted if axis == 0 else x_sorted
 
-        med_ind = len(arrWithMedian) // 2
+
+        med_ind = ceil(len(arrWithMedian) / 2)
         leftArr1, rightArr1 = arrWithMedian[:med_ind], arrWithMedian[med_ind:]  # TODO change name of the array for something more suitable
 
         splitPointCoordinate = leftArr1[-1][axis]
@@ -45,3 +60,26 @@ class KDTree:
             return leftArr1, rightArr1, leftArr2, rightArr2
         else:
             return leftArr2, rightArr2, leftArr1, rightArr2
+
+    def printTree(self, node):
+        if node.__class__ == LeafNode:
+            print(node.point)
+            return
+
+        self.printTree(node.left)
+        print(node.splitCoord)
+        self.printTree(node.right)
+
+
+    # def search(self, node, searchRange, nodeRange=Range(-inf, -inf, inf, inf)):
+    #     if node.__class__ == LeafNode:
+    #         return node.point
+    #
+    #
+    #     if
+
+
+
+testTree = KDTree([(3,1), (1,2), (6,0), (7,5), (4,3)])
+testTree.printTree(testTree.kdTreeRoot)
+
